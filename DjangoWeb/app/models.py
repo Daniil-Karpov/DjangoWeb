7b5a9aa770a1db180c77a5c6c222ee3953ee01d6
MarkTylov <Tylov15AA@gmail.com>
2025-04-24 13:21:08 +0300

    Добавьте файлы проекта.

---
+++ DjangoWeb/app/models.py
@@ -0,0 +1,51 @@
+"""
+Definition of models.
+"""
+
+
+from django.contrib import admin
+from datetime import datetime
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db import models
+#from .models import Comment 
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique_for_date="posted", verbose_name="Заголовки")
+    description = models.TextField(verbose_name = "Краткое содержание")
+    content = models.TextField(verbose_name="Полное содержание")
+    poster = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Опубликована")
+    author = models.ForeignKey(User, null=True, blank=True, on_delete = models.SET_NULL, verbose_name = "Автор")
+    image = models.FileField(default='temp.jpg', verbose_name ='Путь к картинке' )
+
+    def get_absolute_url(self):
+        return reverse("blogpost", args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table="Postes"
+        ordering = ["-poster"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name="Текст комментария")
+    date = models.DateTimeField(default=datetime.now, verbose_name="Дата комментария")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, verbose_name="Статья")
+
+    def __str__(self):
+        return f'Комментарий от {self.author.username} к статье "{self.post.title}"'
+
+    class Meta:
+        ordering = ['-date']  
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+admin.site.register(Blog)
+admin.site.register(Comment)
